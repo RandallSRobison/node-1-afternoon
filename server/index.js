bef998517180c25422fe4b839eb80fa286e911bf
Spence Robison <spence@Spences-MacBook-Pro.local>
2019-06-21 15:40:14 -0600

    finished

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const getProducts = require('./getProducts')
+const getProduct = require('./getProduct')
+
+const app = express();
+
+const port = 5050;
+
+app.get("/api/products", getProducts)
+
+app.get("/api/product/:id", getProduct)
+
+app.listen(port, () => {
+    console.log('server listening on port', port);
+})
+
+
